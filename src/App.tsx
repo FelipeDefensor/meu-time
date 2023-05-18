6dece073a4429b2f585e24df71fc7e920e1a8461
jiraffarij <felipe.martins4@hotmail.com>
2023-05-18 16:53:23 -0300

    request countries from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import * as React from "react";
 import "./App.css";
+import axiosInstance from "./axios";
 
 function App() {
+  const getCountries = () => {
+    const _getCountries = async () => {
+      try {
+        const response = await axiosInstance.get("countries");
+        console.log(response);
+      } catch (err) {
+        if (err.response.status == 403) {
+          console.log("Chave da API inválida.");
+        }
+        console.log(err);
+      }
+    };
+    _getCountries();
+  };
+
+  React.useEffect(getCountries, []);
   return (
     <>
       <h1>API-Football</h1>
