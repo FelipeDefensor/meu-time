df4785ee12b607269aa9a036612d2b0c2d531d43
jiraffarij <felipe.martins4@hotmail.com>
2023-05-22 17:21:17 -0300

    configure for testing with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: './tests/setup.ts'
+  }, 
   plugins: [react()],
 })
