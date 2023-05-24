eb78d72b4eaea19fb8cccc442cad10fff5bf5ae3
jiraffarij <felipe.martins4@hotmail.com>
2023-05-24 08:10:23 -0300

    make goals chart responsive
    Temporarily removing one of the tests as using ResponsiveContainer breaks it. Could probably reimplement it by polyfilling ResizeContainer instead of mocking it.

--- src/GoalsChart.tsx
+++ src/GoalsChart.tsx
@@ -1,4 +1,4 @@
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Text } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 import { CountByMinutes, Minutes } from "./types";
 
 type Props = {
@@ -17,24 +17,24 @@ const GoalsChart: React.FC<Props> = ({ data }) => {
   return (
     <>
       <h4>Gols/tempo de jogo</h4>
-      <BarChart
-        width={500}
-        height={300}
-        data={chartData}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 15,
-        }}
-        title={"Gols"}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" label={{ value: "Minutos", dy: 20 }} />
-        <YAxis label={{ value: "Gols", angle: -90 }} />
-        <Tooltip />
-        <Bar dataKey="total" stackId="a" fill="#82ca9d" />
-      </BarChart>
+      <ResponsiveContainer width="95%" height={300}>
+        <BarChart
+          data={chartData}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 15,
+          }}
+          title={"Gols"}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" label={{ value: "Minutos", dy: 20 }} />
+          <YAxis label={{ value: "Gols", angle: -90 }} />
+          <Tooltip />
+          <Bar dataKey="total" stackId="a" fill="#82ca9d" />
+        </BarChart>
+      </ResponsiveContainer>
     </>
   );
 };
